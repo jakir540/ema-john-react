dad6b4c31c6a779d7c69550debf2d2fa3e93108f
md jakir hossain <jakirhossainmd540@gmail.com>
2023-03-28 11:58:42 +0600

    Local Storage Functionality added

---
+++ src/utilites.jsx
@@ -0,0 +1,45 @@
+const addToDb =(id)=>{
+    let shoppingCart = getShoppingCart();
+    let quantity = shoppingCart[id];
+    if (!quantity) {
+        shoppingCart[id] = 1;
+        
+    }else{
+        const newQuantity = quantity +1;
+        shoppingCart[id] = newQuantity;
+    }
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart))
+
+}
+
+const removeFromDb =(id)=>{
+    const shoppingCart = getShoppingCart();
+    if (id in shoppingCart) {
+        delete shoppingCart[id];
+        localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
+    }
+
+}
+
+
+const getShoppingCart =(id)=>{
+let shoppingCart = {};
+const storeCart = localStorage.getItem('shopping-cart');
+if (storeCart) {
+    shoppingCart = JSON.parse(storeCart);
+    
+}
+return shoppingCart;
+
+}
+
+const deleteShoppingCart =()=>{
+    localStorage.removeItem('shopping-cart')
+}
+
+export {
+    addToDb,
+    deleteShoppingCart,
+    getShoppingCart,
+    removeFromDb,
+}
